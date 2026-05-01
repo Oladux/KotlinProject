24cbb9d787479a932247d38c7f4ff89ba2f8724c
Stepan <stepasemin@mail.ru>
2026-05-01 20:19:19 +0300

    Основы разметки

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -4,23 +4,56 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draganddrop.DragData
 import androidx.compose.ui.tooling.preview.Preview
 import org.jetbrains.compose.resources.painterResource
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
 
 @Composable
-@Preview
+fun ShoppingListElement(description: String) {
+    Text(description)
+}
+@Composable
 fun App() {
-    Text("Hello")
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Список покупок: "
+        )
+        val someList = remember { listOf("Молоко", "Яйца", "Хлеб") }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 6.dp)
+        ) {
+        LazyRow(horizontalArrangement = Arrangement.SpaceBetween) {
+            items(someList) {
+                ShoppingListElement(it)
+                Text(", ")
+            }
+        }
+    }
+    }
 }
