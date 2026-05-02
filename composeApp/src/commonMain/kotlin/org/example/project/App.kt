f2f0c52eeaccd9788fbf214d5ae115b571a4fc6a
Stepan <stepasemin@mail.ru>
2026-05-02 15:27:37 +0300

    Системная тематика оформления

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,5 +1,7 @@
 package org.example.project
 
+import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,52 +17,17 @@ import androidx.compose.ui.unit.dp
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.*
 import org.jetbrains.compose.resources.stringResource
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 
 data class ShoppingListItem(
     val description: String,
     val bought: Boolean = false
 )
 
-@Composable
-fun App() {
-    val milkText = stringResource(Res.string.milk)
-    val flourText = stringResource(Res.string.flour)
-    val shoppingList = remember {
-        mutableStateListOf(ShoppingListItem(milkText), ShoppingListItem(flourText))
-    }
-    var newItemDesc by remember { mutableStateOf("") }
-    LazyColumn {
-        item {
-            OutlinedTextField(
-                value = newItemDesc, onValueChange = { newItemDesc = it },
-                modifier = Modifier.padding(8.dp),
-                label = {
-                    Text("Название продукта")
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        if (newItemDesc.isNotBlank()) {
-                            shoppingList.add(ShoppingListItem(newItemDesc.trim()))
-                            newItemDesc = ""
-                        }
-                    }) {
-                        Icon(Icons.Default.Add, contentDescription = "Добавить")
-                    }
-                })
-        }
-        itemsIndexed(shoppingList) { i, item ->
-            ShoppingListElement(
-                item,
-                onBoughtChange = {
-                    shoppingList[i] = item.copy(bought = it)
-                },
-                onDelete = {
-                    shoppingList.removeAt(i)
-                }
-            )
-        }
-    }
-}
+val LightScheme = lightColorScheme()
+val DarkScheme = darkColorScheme()
 
 @Composable
 fun ShoppingListElement(item: ShoppingListItem, onBoughtChange: (Boolean) -> Unit, onDelete: () -> Unit) {
@@ -72,6 +39,72 @@ fun ShoppingListElement(item: ShoppingListItem, onBoughtChange: (Boolean) -> Uni
         Text(item.description, Modifier.weight(1f))
         IconButton(onClick = onDelete) {
             Icon(Icons.Default.Delete, contentDescription = "Удалить")
+        }
+    }
+}
+
+@Composable
+fun App() {
+    val darkThemeFlag = isSystemInDarkTheme()
+    val theme = if (darkThemeFlag)
+        DarkScheme
+    else
+        LightScheme
+    val milkText = stringResource(Res.string.milk)
+    val flourText = stringResource(Res.string.flour)
+    val shoppingList = remember {
+        mutableStateListOf(ShoppingListItem(milkText), ShoppingListItem(flourText))
+    }
+
+    MaterialTheme(colorScheme = theme) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier
+                    .safeDrawingPadding()
+                    .padding(16.dp)
+            ) {
+
+                Text(
+                    text = "🛒 Список покупок",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                var newItemDesc by remember { mutableStateOf("") }
+                LazyColumn {
+                    item {
+                        OutlinedTextField(
+                            value = newItemDesc, onValueChange = { newItemDesc = it },
+                            modifier = Modifier.padding(8.dp),
+                            label = {
+                                Text("Название продукта")
+                            },
+                            trailingIcon = {
+                                IconButton(onClick = {
+                                    if (newItemDesc.isNotBlank()) {
+                                        shoppingList.add(ShoppingListItem(newItemDesc.trim()))
+                                        newItemDesc = ""
+                                    }
+                                }) {
+                                    Icon(Icons.Default.Add, contentDescription = "Добавить")
+                                }
+                            })
+                    }
+                    itemsIndexed(shoppingList) { i, item ->
+                        ShoppingListElement(
+                            item,
+                            onBoughtChange = {
+                                shoppingList[i] = item.copy(bought = it)
+                            },
+                            onDelete = {
+                                shoppingList.removeAt(i)
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 }
